1988d4fc77d7da0101f98199bafa0f54ba7dc2db
Rooney H. Osman <dev@ronios.de>
2022-05-15 22:43:07 +0200

    generate summary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from "@angular/common/http";
 import {StocksService} from "./services/stocks.service";
+import { SummaryComponent } from './components/summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
