097063cc871b50cff8f773dd761698e24e665e84
Rooney H. Osman <dev@ronios.de>
2022-05-15 22:37:51 +0200

    register the new StocksService with the providers. Import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule} from "@angular/common/http";
+import {StocksService} from "./services/stocks.service";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [StocksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
