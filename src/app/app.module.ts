e251917424a0b48b4bb92b13383865f69757f591
Rooney H. Osman <dev@ronios.de>
2022-05-22 14:34:34 +0200

    create manage component
    a new component for the manage view.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpClientModule} from "@angular/common/http";
 import {StocksService} from "./services/stocks.service";
 import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
-    DashboardComponent
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
