a1197059feac0114da431680ff1c39450471b344
Rooney H. Osman <dev@ronios.de>
2022-05-22 15:09:15 +0200

    update manange.component.ts
    comprise the additional methods and setup required for manage view

--- src/app/components/manage/manage.component.ts
+++ src/app/components/manage/manage.component.ts
@@ -1,5 +1,8 @@
+// Imports dependencies
 import { Component, OnInit } from '@angular/core';
+import {StocksService} from "../../services/stocks.service";
 
+// Declares component metadata
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
@@ -7,7 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageComponent implements OnInit {
 
-  constructor() { }
+  // Defines class and two properties for storing the array of symbols and a string for input
+  symbols?: Array<string>;
+
+  stock: string = "";
+
+  constructor(private service: StocksService) {
+    // Gets the current list of symbols during class instantiation
+    // This doesn't require the OnInit lifecycle hook, because it's a synchronous request to get data that exists in memory.
+    this.symbols = service.get();
+  }
+
+  // Method to add a new stock to the list
+  add(){
+    this.symbols = this.service.add(this.stock.toUpperCase());
+  }
+
+  // Method to remove a stock symbol from the list
+  remove(symbol:string){
+    this.symbols = this.service.remove(symbol);
+  }
+
 
   ngOnInit(): void {
   }
